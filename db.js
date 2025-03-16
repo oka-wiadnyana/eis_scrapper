7c8b92713f72d0338d488d472b77568d8c165a68
unknown <onsdee86@gmail.com>
2025-03-16 21:05:05 +0800

    fix: vulnerable db

--- db.js
+++ db.js
@@ -1,12 +1,17 @@
-// inisisalisasi database
 const mysql = require("mysql");
+require("dotenv").config();
+const dbHost = process.env.DB_HOST;
+const dbUsername = process.env.DB_USERNAME;
+const dbPassword = process.env.DB_PASSWORD;
+const dbDatabase = process.env.DB_DATABASE;
+const dbPort = process.env.DB_PORT;
 const db = mysql.createPool({
   // sesuaikan konfigurasi dengan server
-  host: "onsdeeapp.my.id",
-  user: "onsdeeap_vio",
-  password: "vio19092019",
-  database: "onsdeeap_myperkara_tabanan",
-  port: 3306,
+  host: dbHost,
+  user: dbUsername,
+  password: dbPassword,
+  database: dbDatabase,
+  port: dbPort,
 });
 
 module.exports = db;
